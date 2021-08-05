6c20efe4b95836416e3fac76c4d8a53ce0a4f416
Mikhail Trunov <mikhail.trunov@gmail.com>
2021-08-05 18:26:24 +0300

    Added unittests

--- main.py
+++ main.py
@@ -3,9 +3,7 @@ from auxiliary_funcs import clear_data, is_json_valid, regex_validation
 import json, unittest
 
 
-valid_json_string_to_parse = '{"name": "Мишка", "email": "dsads@dsfsdf.ru", "age": 32}'
-invalid_json_string_to_parse = '{"name": "Мишка", "email": "fsdafdasfdsa@fdsafsda.ru"}'
-invalid_json_output = '{"name": "Мишка", "email": "erterwtre", "age": 32}'
+
 
 
 @valid_all(input_validation=is_json_valid, output_validation=regex_validation, default_behavior=clear_data)
--- tests.py
+++ tests.py
@@ -2,9 +2,19 @@
 
 import unittest
 from main import all_ok, main
+from cust_exceptions import (FailRepeatTimesError,
+    InputParameterVerificationError, 
+    ResultVerificationError)
 
+valid_json_string_to_parse = '{"name": "Мишка", "email": "dsads@dsfsdf.ru", "age": 32}'
+invalid_json_string_to_parse = '{"name": "Мишка", "email": "fsdafdasfdsa@fdsafsda.ru"}'
+invalid_json_output = '{"name": "Мишка", "email": "erterwtre", "age": 32}'
 
 class CheckDecoratorTest(unittest.TestCase):
 
     def test_error(self):
-        self.assertRaises(main(data=invalid_json_string_to_parse, schema="schema.json"), Exception)
+        with self.assertRaises(InputParameterVerificationError):
+            main(data=invalid_json_string_to_parse, schema="schema.json")
+
+if __name__ == '__main__':
+    unittest.main()
